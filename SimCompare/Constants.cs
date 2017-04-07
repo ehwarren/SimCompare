ded0b5d6ac62f9a8740a9b6f29dbecae1f3028fc
Austin Warren <austinjwarren@gmail.com>
2017-04-07 14:33:41 -0700

    Added support for change statistics. Fixed large bug with null pointer exceptions causing wrong counts.

--- SimCompare/Constants.cs
+++ SimCompare/Constants.cs
@@ -10,7 +10,7 @@ namespace SimCompare
     {
         public static string TITLE = "SimCompare";
         public static string VERSION = "1";
-        public static string REVISION = "1";
+        public static string REVISION = "6";
 
         public static string OUTPUT_FOLDER = "outputs";
     }
--- SimCompare/FileMan.cs
+++ SimCompare/FileMan.cs
@@ -83,15 +83,25 @@ namespace SimCompare
             //Each of the list values will eventuall be one line in our CSV file.
             List<String> values = new List<String>();
 
+            //The following is used to track the volume and price of each grade in each simulation
+            List<String>[] grades = new List<String>[modifiedFilesToParse.Length + 1];
+            Dictionary<String, int>[] numberOfPieces = new Dictionary<string, int>[modifiedFilesToParse.Length + 1];
+            Dictionary<String, float>[] volumeOfPieces = new Dictionary<string, float>[modifiedFilesToParse.Length + 1];
+            List<int> numberOfLugs = new List<int>();
+            List<int> numberOfLugsWithDifferentSolution = new List<int>();
+            List<float> totalPrice = new List<float>();
+            List<float> totalVolume = new List<float>();
+
+
             String currentLine = "";
             //Add the tags for each of the simulations
-            currentLine = "ORIGINAL DATA" + seperator + seperator + seperator + seperator + seperator + seperator + seperator + seperator;
+            currentLine = "ORIGINAL DATA" + addSeperator(8);
             for (int i = 0; i < modifiedFilesToParse.Length; i++)
             {
                 currentLine += "SIMULATION ";
                 currentLine += i + 1;
                 currentLine += " " + modifiedFilesToParse[i];
-                currentLine += seperator + seperator + seperator + seperator + seperator + seperator + seperator + seperator + seperator;
+                currentLine += addSeperator(9);
             }
             values.Add(currentLine);
             //add the column headers for each of the simulations
@@ -101,7 +111,14 @@ namespace SimCompare
                             " grade" + seperator +
                             " price" + seperator + 
                             " volume" + seperator +
-                            " zPosition" + seperator + seperator;
+                            " zPosition" + addSeperator(2);
+            grades[0] = new List<string>();
+            numberOfPieces[0] = new Dictionary<string, int>();
+            volumeOfPieces[0] = new Dictionary<string, float>();
+            totalPrice.Add(0);
+            totalVolume.Add(0);
+            numberOfLugs.Add(0);
+            numberOfLugsWithDifferentSolution.Add(0);
             for (int i = 0; i < modifiedFilesToParse.Length; i++)
             {
                 currentLine += "shapeId" + seperator +
@@ -111,7 +128,14 @@ namespace SimCompare
                                 " price" + seperator +
                                 " volume" + seperator +
                                 " zPosition" + seperator +
-                                " isDifferent" + seperator + seperator;
+                                " isDifferent" + addSeperator(2);
+                grades[i+1] = new List<string>();
+                numberOfPieces[i+1] = new Dictionary<string, int>();
+                volumeOfPieces[i+1] = new Dictionary<string, float>();
+                totalPrice.Add(0);
+                totalVolume.Add(0);
+                numberOfLugs.Add(0);
+                numberOfLugsWithDifferentSolution.Add(0);
             }
             values.Add(currentLine);
             //clear our currentLine variable because we've added it to the list already
@@ -127,6 +151,10 @@ namespace SimCompare
             //Loop through each of the files we want to compare the original for..
             Console.WriteLine("Parsing the files...");
             Console.WriteLine(orig.Root.Elements().Count());
+
+            int[] lastLugIndexUsedForDifference = new int[changes.Length];
+            int[] lastLugIndexUsedForCount = new int[changes.Length];
+
             //Loop through all of the XML data..
             for (int j = 0; j < orig.Root.Elements().Count(); j++)
             {
@@ -154,21 +182,37 @@ namespace SimCompare
                     }
                     else
                     {
-                        entry += seperator + seperator;
+                        entry += addSeperator(2);
                     }
 
                     entry += orig_data.Element("price").Value + seperator;
                     entry += orig_data.Element("volume").Value + seperator;
-                    entry += orig_data.Element("zPosition").Value + seperator + seperator;
+                    entry += orig_data.Element("zPosition").Value + addSeperator(2);
+
+                    if (orig_data.Elements("grade").Any())
+                    {
+                        if (!grades[0].Contains(orig_data.Element("grade").Value))
+                        {
+                            grades[0].Add(orig_data.Element("grade").Value);
+                            numberOfPieces[0][orig_data.Element("grade").Value] = 0;
+                            volumeOfPieces[0][orig_data.Element("grade").Value] = 0;
+                        }
+                        numberOfPieces[0][orig_data.Element("grade").Value]++;
+                        volumeOfPieces[0][orig_data.Element("grade").Value] += float.Parse(orig_data.Element("volume").Value);
+                    }
+
+                    numberOfLugs[0]++;
+                    totalPrice[0] += float.Parse(orig_data.Element("price").Value);
+                    totalVolume[0] += float.Parse(orig_data.Element("volume").Value);
+
                 }
                 catch { }
-               
 
+                int numSim = 1;
                 //now we loop through each of our changed files and add the entry to the list
                 for (int sim = 0; sim < changes.Length; sim++)
                 {
                     isDifferent = false;    //BugFix1 - Didn't clear the variable, so if the first file compared was different it would assume all the rest were too...
-
                     XElement sim_data = changes[sim].Root.Elements().ElementAt(j);
                     try
                     {
@@ -183,7 +227,7 @@ namespace SimCompare
                         }
                         else
                         {
-                            entry += seperator + seperator;
+                            entry += addSeperator(2);
                         }
 
                         entry += sim_data.Element("price").Value + seperator;
@@ -192,8 +236,14 @@ namespace SimCompare
 
                         if (sim_data.Elements("name").Any() && orig_data.Elements("name").Any())
                         {
+                            //If they both contain a name, we need to see if that name changed to mark it as different.
                             if (sim_data.Element("name").Value != orig_data.Element("name").Value)
                                 isDifferent = true;
+                        }
+                        else if ( sim_data.Elements("name").Any() || orig_data.Elements("name").Any() )
+                        {
+                            //If one contains a name and the other doesn't this means that we had a cut-n-two in 1 and not in the other.
+                            isDifferent = true;
                         }
                         if (useZPosAsDifference == true)
                         {
@@ -202,13 +252,52 @@ namespace SimCompare
                                 if (sim_data.Element("zPosition").Value != orig_data.Element("zPosition").Value)
                                     isDifferent = true;
                             }
-                        }                        
+                        }
+
+                        if(sim_data.Elements("grade").Any())
+                        {
+                            if (!grades[numSim].Contains(sim_data.Element("grade").Value))
+                            {
+                                grades[numSim].Add(sim_data.Element("grade").Value);
+                                numberOfPieces[numSim][sim_data.Element("grade").Value] = 0;
+                                volumeOfPieces[numSim][sim_data.Element("grade").Value] = 0;
+                            }
+                            numberOfPieces[numSim][sim_data.Element("grade").Value]++;
+                            volumeOfPieces[numSim][sim_data.Element("grade").Value] += float.Parse(sim_data.Element("volume").Value);
+                        }
+
+                        totalPrice[numSim] += float.Parse(sim_data.Element("price").Value);
+                        totalVolume[numSim] += float.Parse(sim_data.Element("volume").Value);
+
+                        //Check to see if we need to increment our numberOfLugsWithDifferentSolution counter
+                        int currentLugIndex = int.Parse(sim_data.Element("lugIndex").Value);
+                        if (lastLugIndexUsedForCount[numSim-1] != currentLugIndex)
+                        {
+                            numberOfLugs[numSim]++;
+                            lastLugIndexUsedForCount[numSim - 1] = currentLugIndex;
+                        }
+                        if (lastLugIndexUsedForDifference[numSim - 1] != currentLugIndex)
+                        {
+                            if(isDifferent)
+                            {
+                                numberOfLugsWithDifferentSolution[numSim]++;
+                                lastLugIndexUsedForDifference[numSim - 1] = currentLugIndex;
+                            }
+                        }
+                        
+                        
                     }
                     catch { }
                     if (isDifferent)
-                        entry += "1" + seperator + seperator;
+                    {
+                        entry += "1" + addSeperator(2);   
+                    }
                     else
-                        entry += seperator + seperator;
+                    {
+                        entry += addSeperator(2);
+                    }
+
+                    numSim++;
                 }
 
                 values.Add(entry);
@@ -216,6 +305,139 @@ namespace SimCompare
 
                 currentLine = "";
 
+            }
+            //Now we add the volume/piece count data to the report
+            values.Add("");
+            currentLine = addSeperator(3) + "Total Price" + addSeperator(2) + "Total Volume" + addSeperator(3);
+            for (int i = 0; i < changes.Length; i++)
+            {
+                currentLine += addSeperator(3) + "Total Price" + addSeperator(2) + "Total Volume" + addSeperator(4);
+            }
+            values.Add(currentLine);
+
+            //Add total price/volume
+            currentLine = "";
+            for(int i=0; i<numberOfLugs.Count; i++)
+            {
+                currentLine += addSeperator(3);
+                currentLine += totalPrice[i];
+                currentLine += addSeperator(2);
+                currentLine += totalVolume[i];
+                currentLine += addSeperator(3);
+                if (i > 0)
+                    currentLine += seperator;
+            }
+            values.Add(currentLine);
+
+            //Add changes in price/volume
+            currentLine = "";
+            for (int i = 0; i < numberOfLugs.Count; i++)
+            {
+                if(i>0)
+                {
+                    currentLine += addSeperator(2) + "Change:" + seperator;
+                    currentLine += totalPrice[i] - totalPrice[0];
+                    currentLine += addSeperator(2);
+                    currentLine += totalVolume[i] - totalVolume[0];
+                    currentLine += addSeperator(4);
+                }
+                else
+                {
+                    currentLine += addSeperator(8);
+                }
+            }
+            values.Add(currentLine);
+            values.Add("");
+            values.Add("");
+
+            //Add total number of lugs
+            currentLine = "";
+            for (int i = 0; i < numberOfLugs.Count; i++)
+            {
+                if (i > 0)
+                {
+                    currentLine += addSeperator(2) + "Number of lugs in this sample:" + addSeperator(3);
+                    currentLine += numberOfLugs[i];
+                    currentLine += addSeperator(4);
+                }
+                else
+                {
+                    currentLine += addSeperator(8);
+                }
+            }
+            values.Add(currentLine);
+
+            //Add total number of changes
+            currentLine = "";
+            for (int i = 0; i < numberOfLugs.Count; i++)
+            {
+                if (i > 0)
+                {
+                    currentLine += seperator + "Number of lugs with different solution:" + addSeperator(4);
+                    currentLine += numberOfLugsWithDifferentSolution[i];
+                    currentLine += addSeperator(4);
+                }
+                else
+                {
+                    currentLine += addSeperator(8);
+                }
+            }
+            values.Add(currentLine);
+
+            //Add percentage of changes
+            currentLine = "";
+            for (int i = 0; i < numberOfLugs.Count; i++)
+            {
+                if (i > 0)
+                {
+                    currentLine += seperator + "Percent of lugs with different solution:" + addSeperator(4);
+                    currentLine += ((float)numberOfLugsWithDifferentSolution[i]/numberOfLugs[i])*100;
+                    currentLine += "%";
+                    currentLine += addSeperator(4);
+                }
+                else
+                {
+                    currentLine += addSeperator(8);
+                }
+            }
+            values.Add(currentLine);
+            values.Add("");
+            values.Add("");
+            values.Add("");
+            currentLine = addSeperator(3) + "Number PCS" + addSeperator(2) + "Volume of Grade" + addSeperator(3);
+            for(int i=0; i<changes.Length; i++)
+            {
+                currentLine += addSeperator(3) + "Number PCS" + addSeperator(2) + "Volume of Grade" + addSeperator(4);
+            }
+            values.Add(currentLine);
+
+            bool stillCounting = true;
+            int currentGrade = 0;
+            while (stillCounting)
+            {
+                currentLine = addSeperator(2);
+                stillCounting = false;
+                for(int currentSim = 0; currentSim < grades.Length; currentSim++)
+                {
+                    if (currentGrade < grades[currentSim].Count)
+                    {
+                        currentLine += grades[currentSim][currentGrade];
+                        currentLine += seperator + numberOfPieces[currentSim][grades[currentSim][currentGrade]];
+                        currentLine += addSeperator(2) + volumeOfPieces[currentSim][grades[currentSim][currentGrade]];
+                        currentLine += addSeperator(3);
+                        stillCounting = true;
+                    }
+                    else
+                    {
+                        currentLine += addSeperator(6);
+                    }
+                    currentLine += addSeperator(2);
+                    if (currentSim > 0)
+                        currentLine += seperator;
+                }
+                values.Add(currentLine);
+                currentGrade++;
+                
             }
             string outputDir = Directory.GetCurrentDirectory() + "/" + Constants.OUTPUT_FOLDER;
             MessageBox.Show(writeCSV(values, originalFileToParse[0]));
@@ -386,6 +608,16 @@ namespace SimCompare
                .Elements("Solution")
                .OrderByDescending(p => p.Element("lugIndex").Value);
             return new XDocument(new XElement("Data", tt));
+        }
+
+        private string addSeperator(int n)
+        {
+            string temp = "";
+            for(int i=0; i<n; i++)
+            {
+                temp += seperator;
+            }
+            return temp;
         }
     }
 }
